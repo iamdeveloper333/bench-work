531ba95bd8f3455eaff7910f303b3ff0d912c40f
aarav.gupta <aarav.gupta@tothenew.com>
2024-04-16 17:55:18 +0530

    finished project using app router

--- nextjs-work/app/meals/page.tsx
+++ nextjs-work/app/meals/page.tsx
@@ -1,11 +1,17 @@
+import { Suspense } from "react";
 import Link from "next/link";
 
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
-async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -21,10 +27,12 @@ async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
 }
-
-export default MealsPage;
